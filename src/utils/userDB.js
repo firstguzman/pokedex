6b9971c395d7ca9e226bccdb0ed5cf48edaeb1a9
Cesar Guzman <cesar_guzman2000@hotmail.com>
2022-09-20 16:19:08 -0400

    Ref: My account screen

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: 'firstguzman',
+  password: '123456789'
+}
+
+export const userDetails = {
+  username: 'firstguzman',
+  firstName: 'César',
+  lastName: 'Guzmán Garrido',
+  email: 'cesar_guzman2000@hotmail.com'
+}
