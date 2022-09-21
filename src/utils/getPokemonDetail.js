292ed967d7bda04e775c3c3fe9c8091846d61e70
César <cesar_guzman2000@hotmail.com>
2022-09-21 00:48:20 -0400

    Fix: bug when want to get details by id

--- src/utils/getPokemonDetail.js
+++ src/utils/getPokemonDetail.js
@@ -1,15 +1,20 @@
-import { getPokemonsDetailsByUrlApi } from "../api/pokemon"
+import {
+  getPokemonDetailsApi,
+  getPokemonsDetailsByUrlApi
+} from '../api/pokemon'
 
 export async function getPokemonDetail(pokemons) {
   const pokemonDetail = []
 
   for await (const pokemon of pokemons) {
     const identifier = pokemon.url || pokemon
-    const pokemonDetails = await getPokemonsDetailsByUrlApi(identifier)
+    const pokemonDetails = pokemon.url
+      ? await getPokemonsDetailsByUrlApi(identifier)
+      : await getPokemonDetailsApi(pokemon)
     pokemonDetail.push({
       id: pokemonDetails.id,
       name: pokemonDetails.name,
-      type: pokemonDetails.types.map((type) => type.type.name),
+      type: pokemonDetails.types[0].type.name,
       order: pokemonDetails.order,
       image: pokemonDetails.sprites.other['official-artwork'].front_default
     })
